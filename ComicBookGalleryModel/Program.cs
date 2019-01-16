8d563f5588caa660202fbcefe21e402dab15cdc9
Grzegorz Miazga <greal@wp.pl>
2019-01-16 09:53:10 +0100

    .

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -1,11 +1,7 @@
-﻿using ComicBookGalleryModel.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Data.Entity;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ComicBookGalleryModel
 {
